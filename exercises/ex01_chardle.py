fc4bca33ad48db0b0573c9ec288e7df77cd2a396
Claire Miller <cmillr@live.unc.edu>
2022-01-21 16:48:25 -0500

    ex01changes

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,14 +4,28 @@ __author__ = "730317751"
 
 word: str = input("Enter a 5-character word: ")
 character: str = input("Enter a single character: ")
+matches: int = (0)
 print("Searching for", character, "in", word)
 if word[0] == character:
     print(character, "found at index 0")
+    matches = matches + 1
 if word[1] == character:
     print(character, "found at index 1")
+    matches = matches + 1
 if word[2] == character:
     print(character, "found at index 2")
+    matches = matches + 1
 if word[3] == character:
     print(character, "found at index 3")
+    matches = matches + 1
 if word[4] == character:
     print(character, "found at index 4")
+    matches = matches + 1
+
+if matches == 0:
+    print("No instances of", character, "found in", word)
+else:
+    if matches == 1:
+        print(matches, "instance of", character, "found in", word)
+    else:
+        print(matches, "instances found in", word)
