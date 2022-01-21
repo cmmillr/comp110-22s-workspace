5ca7e14fa2e1a6c9a7d40ee5fa19a0e6e3b3348f
Claire Miller <cmillr@live.unc.edu>
2022-01-20 22:24:32 -0500

    editspractice

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,7 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730317751"
+
+word: str = input("Enter a 5-character word: ")
+character: str = input("Enter a single character: ")
+print("Searching for", character, "in", word)
---
+++ lessons/quiz0practice2.py
@@ -0,0 +1,5 @@
+"""wtf is happening with this problem."""
+
+b: str = "3.14"
+a: str = "qwerty"
+len(b)
