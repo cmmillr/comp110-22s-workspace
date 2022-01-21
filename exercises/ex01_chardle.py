deaf8fa482ac304e7bb108d986d9c4c3ba54b484
Claire Miller <cmillr@live.unc.edu>
2022-01-21 16:08:14 -0500

    ididit

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,3 +5,13 @@ __author__ = "730317751"
 word: str = input("Enter a 5-character word: ")
 character: str = input("Enter a single character: ")
 print("Searching for", character, "in", word)
+if word[0] == character:
+    print(character, "found at index 0")
+if word[1] == character:
+    print(character, "found at index 1")
+if word[2] == character:
+    print(character, "found at index 2")
+if word[3] == character:
+    print(character, "found at index 3")
+if word[4] == character:
+    print(character, "found at index 4")
